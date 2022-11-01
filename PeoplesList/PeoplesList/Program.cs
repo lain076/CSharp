b84cc2f9ab52e0a4cd4a1417c650580c2c1179f5
perfectBlue <djpcacc@gmail.com>
2022-11-01 14:19:09 +0100

    PeopleList solution creation

---
+++ PeoplesList/PeoplesList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PeoplesList
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello samsam");
+        }
+    }
+}
