3bdee8bbbc1a3d956499be7e825593f345be1b72
perfectBlue <djpcacc@gmail.com>
2022-11-02 16:03:22 +0100

    Add create list function

--- PeoplesList/PeoplesList/Program.cs
+++ PeoplesList/PeoplesList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using peoplesList;
 
 namespace PeoplesList
@@ -7,13 +8,17 @@ namespace PeoplesList
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello samsam");
+            var peoples = CreateList(); 
             Console.ReadLine();
+        }
 
-            var people = new People("Sam","Student",15);
-            
-            people.PrintPeopleInformations();
-            Console.ReadLine();
+        public static List<People> CreateList()
+        {
+            var peoples = new List<People>();
+            peoples.Add(new People("Sam", "Student", 15));
+            peoples.Add(new People("David", "Dev", 45));
+            peoples.Add(new People("Grathy", "Secretary", 42));
+            return peoples;     
         }
     }
 }
