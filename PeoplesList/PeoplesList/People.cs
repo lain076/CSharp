c48b38e68438b0bc74e708641ca8fb7d0ee9e64b
perfectBlue <djpcacc@gmail.com>
2022-11-29 13:13:20 +0100

    Use null value for uninitialized job

--- PeoplesList/PeoplesList/People.cs
+++ PeoplesList/PeoplesList/People.cs
@@ -5,24 +5,32 @@ namespace peoplesList
     public class People
     {
         private static short _peopleNumber;
-        
+
         private readonly string _name;
         private readonly string _job;
         private readonly int _age;
         private readonly int _number;
 
-        public People(string name,  int age, string job="No job specified")
+        public People(string name, int age, string job = null)
         {
             _name = name;
             _job = job;
             _age = age;
             _number = _peopleNumber++;
         }
-        
+
         public void PrintPeopleInformations()
         {
             Console.WriteLine($"Name : {_name}");
-            Console.WriteLine($"Job  : {_job}");
+            if(_job == null)
+            {
+                Console.WriteLine("Job  : No job specified");
+            }
+            else
+            {
+                Console.WriteLine($"Job  : {_job}");    
+            }
+            
             Console.WriteLine($"Age  : {_age}");
             Console.WriteLine($"Number  : {_number}");
             Console.WriteLine("-----------------------------");
@@ -31,7 +39,7 @@ namespace peoplesList
 
         public static void ShowTotalOfPeoples()
         {
-            Console.WriteLine($"Peoples totla : {_peopleNumber}");
+            Console.WriteLine($"Peoples total : {_peopleNumber}");
         }
     }
 }
