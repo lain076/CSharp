4917576d2bf236cb74ed1f20327a44328775bd96
perfectBlue <djpcacc@gmail.com>
2022-12-03 16:16:35 +0100

    Add pizza s ingredients

--- PizzaProject/PizzaProject/Pizza.cs
+++ PizzaProject/PizzaProject/Pizza.cs
@@ -2,5 +2,34 @@
 
 public class Pizza
 {
+    private string _name;
+    private float _price;
+    private bool _vegetarian;
+    private List<string> _ingredients;
     
+    public Pizza(string name,float price,List<string> ingredients, bool vegetarian = false)
+    {
+        _name = name;
+        _price = price;
+        _vegetarian = vegetarian;
+        _ingredients = ingredients;
+    }
+
+    public void PrintPizzaInfo()
+    {
+        var vegetarian = string.Empty;
+        if (_vegetarian) vegetarian = "(V)";
+        Console.WriteLine("-------------------------------------------------------");
+        Console.WriteLine($"Pizza { FormatFirstLetter(_name) } : { vegetarian } - { _price.ToString() } €");
+        Console.WriteLine($"Ingredients : { FormatFirstLetter(String.Join(", ",_ingredients)) }");
+        Console.WriteLine("-------------------------------------------------------");
+        Console.WriteLine("");
+        Console.WriteLine("");
+    }
+
+    private static string FormatFirstLetter(string words)
+    {
+        if (string.IsNullOrEmpty(words)) return "";
+        return words.ToUpper()[0] + words.ToLower()[1..];
+    }
 }
--- PizzaProject/PizzaProject/Program.cs
+++ PizzaProject/PizzaProject/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Worlds!");
+﻿using System;
+using System.Text;
+
+namespace PizzaProject // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        private static List<Pizza> Pizzas = new List<Pizza>();
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            CreatePizzas();
+            PrintPizzas();
+        }
+
+        private static void CreatePizzas()
+        {
+            Pizzas.Add(new Pizza("4 fromages", 11.5F,new List<string>(){"Conté","gruyère","fromfrom","parmesant"}));
+            Pizzas.Add(new Pizza("Champignons", 9.5F,new List<string>(){"Fungis","champis","amanite","tomates"}, true));
+            Pizzas.Add(new Pizza("Bolognèse", 10F, new List<string>(){"tomates","poivrons","haché boeuf","oignon"}));
+            Pizzas.Add(new Pizza("4 saisons", 15F,new List<string>(){"Fungis","oignon","courgette","saumon"}));
+            Pizzas.Add(new Pizza("Arabiata", 9F,new List<string>(){"arabia","ail","saucisson","Tomates"}, true));
+        }
+
+        private static void PrintPizzas()
+        {
+            foreach (var pizza in Pizzas)
+            {
+                pizza.PrintPizzaInfo();
+            }
+        }
+    }
+}
